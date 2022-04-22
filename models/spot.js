59fcea0203c51ab385617c60f75c0275d98ec9f9
zuri227 <zuri22780@gmail.com>
2022-04-21 23:26:55 -0400

    Add review schema

--- models/spot.js
+++ models/spot.js
@@ -2,6 +2,13 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const reviewSchema = newSchema({
+  comment: String,
+  rating: {type: Number, min: 1, max: 5, default: 5}
+  },{
+    timestamps: true
+})
+
 const spotSchema = new Schema({
   name: String,
   country: String,
@@ -16,4 +23,11 @@ const spotSchema = new Schema({
   music: String,
   price: Number,
   eats: String,
+  reviews: [reviewSchema]
 })
+
+const Spot = mongoose.model('Spot', spotSchema)
+
+export {
+  Spot
+}
