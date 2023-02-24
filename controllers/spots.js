73652f2d1b0e3374e406df0f6f5ed3dc23005990
zuri227 <zuri22780@gmail.com>
2023-02-24 17:38:56 -0500

    remove auth

--- controllers/spots.js
+++ controllers/spots.js
@@ -10,24 +10,30 @@ function index (req, res) {
   })
 }
 
-// function create (req, res) {
-//   Spot.create(req.body)
-//   .then(spot => res.json(spot))
-//   .catch(err => res.json(err))
-// }
-
-function create(req, res) {
-  req.body.owner = req.user.profile
+function create (req, res) {
   Spot.create(req.body)
   .then(spot => res.json(spot))
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
+  .catch(err => res.json(err))
 }
+
+// function create(req, res) {
+//   req.body.owner = req.user.profile
+//   Spot.create(req.body)
+//   .then(spot => res.json(spot))
+//   .catch(err => {
+//     console.log(err)
+//     res.json(err)
+//   })
+// }
 
 function update (req, res) {
   Spot.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(spot => res.json(spot))
+  .catch(err => res.json(err))
+}
+
+function show(req, res) {
+  Puppy.findById(req.params.id)
   .then(spot => res.json(spot))
   .catch(err => res.json(err))
 }
@@ -42,5 +48,6 @@ export {
   index,
   create,
   update,
+  show,
   deleteSpot as delete
 }
--- routes/spots.js
+++ routes/spots.js
@@ -1,18 +1,20 @@
 import { Router } from 'express'
 import * as spotsCtrl from '../controllers/spots.js'
-import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+// import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
 /*---------- Public Routes ----------*/
 router.get('/', spotsCtrl.index)
-
+router.post('/', spotsCtrl.create)
+router.put('/:id', spotsCtrl.update)
+router.delete('/:id',spotsCtrl.delete)
 
 /*---------- Protected Routes ----------*/
-router.use(decodeUserFromToken)
-router.post('/', checkAuth, spotsCtrl.create)
-router.put('/:id', checkAuth, spotsCtrl.update)
-router.delete('/:id', checkAuth, spotsCtrl.delete)
+// router.use(decodeUserFromToken)
+// router.post('/', checkAuth, spotsCtrl.create)
+// router.put('/:id', checkAuth, spotsCtrl.update)
+// router.delete('/:id', checkAuth, spotsCtrl.delete)
 
 export {
   router
